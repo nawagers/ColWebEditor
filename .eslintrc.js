b1c62f4beb17eb48fcc06bcab85d461d82c89b3a
Nick Wagers <33252537+nawagers@users.noreply.github.com>
2024-11-04 14:13:10 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,26 @@
-import js from "@eslint/js";
-export default [
-    js.configs.recommended,
-    {
-        env: {
-            browser: true,
-        },
-        rules: {
-            quotes: ["error", "double"],
-        },
+// import js from "@eslint/js";
+// export default [
+//     js.configs.recommended,
+//     {
+//         env: {
+//             browser: true,
+//         },
+//         rules: {
+//             quotes: ["error", "double"],
+//         },
+//     },
+// ];
+var MAX_CHARS = 120;
+var SPACES_PER_TAB = 4;
+
+module.exports = {
+
+    'env': {
+        'browser': true,
+
     },
-];
+
+    'rules': {
+        'quotes': ["error", "double"],
+    },
+};
